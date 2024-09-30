dd8d419faf70ed691925c4795205d7ba44eafc7e
mmasser95 <mmasser95@gmail.com>
2024-10-01 00:36:02 +0200

    Vue template with all unplugin libraries

---
+++ vite.config.ts
@@ -0,0 +1,52 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Layouts from 'vite-plugin-vue-layouts'
+import VueRouter from 'unplugin-vue-router/vite'
+import Components from 'unplugin-vue-components/vite'
+import Fonts from 'unplugin-fonts/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+    }),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    Layouts(),
+    AutoImport({
+      imports: [
+        'vue',
+
+        VueRouterAutoImports
+      ],
+      dts: 'src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      },
+      vueTemplate: true,
+    }),
+    Components({
+      dts: 'src/components.d.ts',
+    }),
+    Fonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
